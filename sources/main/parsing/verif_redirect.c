1c5efe54be1658c7461dc6e3b5ba6f566cbb0ea3
RaphyStoll <raphael.ferreira@ik.me>
2025-03-02 21:59:57 +0100

    add/validate type token redirect in and out

--- sources/main/parsing/verif_redirect.c
+++ sources/main/parsing/verif_redirect.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   verif_redirect.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raphalme <raphalme@student.42.fr>          +#+  +:+       +#+        */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 14:30:11 by raphalme          #+#    #+#             */
-/*   Updated: 2025/03/02 16:15:55 by raphalme         ###   ########.fr       */
+/*   Updated: 2025/03/02 21:59:20 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ bool	valid_type_redirect_in(t_token *cur)
 	{
 		if (!(cur->prev->type == WORD && cur->next->type == WORD))
 			return (false);
-		if (!(cur->prev->type == HEREDOC || cur->prev->type == APPEND
+		if (cur->prev->type == HEREDOC || cur->prev->type == APPEND
 				|| cur->prev->type == REDIRECT_IN
-				|| cur->prev->type == REDIRECT_OUT)
-				|| !(cur->next->type == HEREDOC || cur->next->type == APPEND
+				|| cur->prev->type == REDIRECT_OUT
+				|| cur->next->type == HEREDOC || cur->next->type == APPEND
 				|| cur->next->type == REDIRECT_IN
-				|| cur->next->type == REDIRECT_OUT))
+				|| cur->next->type == REDIRECT_OUT)
 			return (false);
-		if (!(cur->prev->type == PIPE || cur->next->type == PIPE))
+		if (cur->prev->type == PIPE || cur->next->type == PIPE)
 			return (false);
-		if(!(cur->prev->type == AND || cur->next->type == OR))
+		if(cur->prev->type == AND || cur->next->type == OR)
 			return (false);
-		if (!(cur->prev->type == O_PARENTHESIS 
-		|| cur->next->type == C_PARENTHESIS))
+		if (cur->prev->type == O_PARENTHESIS 
+		|| cur->next->type == C_PARENTHESIS)
 			return (false);
 	}
 	return (true);
@@ -40,7 +40,22 @@ bool	t_tokenvalid_type_redirect_out(t_token *cur)
 {
 	if (cur->type == REDIRECT_OUT)
 	{
-
+		if (!(cur->prev->type == WORD && cur->next->type == WORD))
+			return (false);
+		if (cur->prev->type == HEREDOC || cur->prev->type == APPEND
+				|| cur->prev->type == REDIRECT_IN
+				|| cur->prev->type == REDIRECT_OUT
+				|| cur->next->type == HEREDOC || cur->next->type == APPEND
+				|| cur->next->type == REDIRECT_IN
+				|| cur->next->type == REDIRECT_OUT)
+			return (false);
+		if (cur->prev->type == PIPE || cur->next->type == PIPE)
+			return (false);
+		if(cur->prev->type == AND || cur->next->type == OR)
+			return (false);
+		if (cur->prev->type == O_PARENTHESIS 
+		|| cur->next->type == C_PARENTHESIS)
+			return (false);
 	}
 	return (true);
 }
