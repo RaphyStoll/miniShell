facfd19bd8b303d83f5ed334dde2614be3409a91
RaphyStoll <raphael.ferreira@ik.me>
2025-03-10 18:03:30 +0100

    add/ free token

--- sources/main/parsing/free.c
+++ sources/main/parsing/free.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 15:32:45 by raphaelferr       #+#    #+#             */
+/*   Updated: 2025/03/10 17:15:26 by raphaelferr      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../../includes/main/lexing.h"
 #include "../../../includes/main/parsing.h"
 
@@ -5,4 +17,20 @@ void free_all(t_token *token)
 {
 	(void)token;
 	return;
+}
+
+// Fonction pour libérer la liste de tokens
+void	free_tokens(t_token *head)
+{
+	t_token	*current;
+	t_token	*next;
+
+	current = head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
 }
