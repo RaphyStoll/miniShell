5d89a89f3645b9e0b615ff981f20650e0276b782
RaphyStoll <raphael.ferreira@ik.me>
2025-03-10 18:04:56 +0100

    fix/ type en void -> bool

--- sources/main/parsing/parsing.c
+++ sources/main/parsing/parsing.c
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 15:32:42 by raphaelferr       #+#    #+#             */
+/*   Updated: 2025/03/10 17:59:31 by raphaelferr      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../../includes/main/parsing.h"
 
 // Fonction d'initialisation du parsing (à implémenter)
-void init_parsing(t_token *tokens) {
+bool init_parsing(t_token *tokens) {
 	//t_command *cmd;
 	
 	if (if_valide_token(tokens))
 	{
 		printf("tokken error\n");
-		return ;
+		return 1;
 	}
+	printf(BLUE BOLD"tokken is ok\n"NC);
 	//cmd = malloc(sizeof(t_command));
-	print_token(tokens);
-	return;
+	return 0;
 }
