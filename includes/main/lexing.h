0a55c71058fe641922026e83d424ffa0be9c8065
Charlye <Charlye@MacBook-Air-2.local>
2025-03-17 18:28:53 +0100

    add/ synthax error pour quote et parenthese

--- includes/main/lexing.h
+++ includes/main/lexing.h
@@ -6,7 +6,7 @@
 /*   By: Charlye <Charlye@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 13:25:29 by chpasqui          #+#    #+#             */
-/*   Updated: 2025/03/13 16:07:59 by Charlye          ###   ########.fr       */
+/*   Updated: 2025/03/17 17:16:37 by Charlye          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <stdbool.h>
-#include "../../lib/libft/header/libft.h"
-#include "../../lib/libft/header/libft_bonus.h"
+# include "../../../includes/main/error_code.h"
+# include "../../lib/libft/header/libft.h"
 
 // Enum for token type
 typedef enum e_type
@@ -36,47 +36,37 @@ typedef enum e_type
 }	t_type;
 
 // Token structure
-typedef struct	s_token
+typedef struct s_token
 {
 	char			*str;
 	t_type			type;
+	bool			in_double_quotes;
 	struct s_token	*next;
 	struct s_token	*prev;
 }	t_token;
 
-typedef struct s_lexer
-{
-	t_token	*lexer;
-	char	*synthax_error;
-}	t_lexer;
-
-// faire une strucutre qui regroupe toutes les variables que jutilise, a voir si jinsere dans s_lexer
-
-typedef enum e_error
-{
-	UNCLOSED_QUOTE,
-	UNCLOSED_PARENTHESIS,
-	MEMORY_ERROR,
-}	t_error;
-
-
-// Function prototypes lexer
-// Function prototypes
+// Lexing
 t_token	*lexer(const char *input);
 t_token	*tokenizing(const char *input);
+
+// Token
+char	*handle_word(const char **input, bool *in_double_quotes);
+char	*get_quoted_word(const char **input, bool *in_dquote);
+char	*get_unquoted_word(const char **input);
 bool	handle_operator(t_token **token_list, const char **input);
 bool	add_operator(t_token **token_list, const char **input, t_type op);
-char	*handle_word(const char **input);
-bool	add_token(t_token **token_list, char *str, t_type op);
+bool	add_token(t_token **token_list, char *str, t_type op, bool dquotes);
+
+// Synthax utils
 t_type	is_operator(const char *input);
 bool	is_symbol(char c);
 bool	is_parenthesis(char c);
+
+//Error handling
 bool	check_unclosed_quotes(const char *input);
 bool	check_unclosed_parentheses(const char *input);
-bool	ft_exit_error(t_token *tokens, t_error code, char *error_token);
+void	ft_exit_error(t_token *tokens, t_error code, char *error_token);
+void	print_error_message(t_error code, char *error_token);
 void	free_all(t_token *token);
-
-// debug
-void	print_token(t_token token);
 
 #endif
