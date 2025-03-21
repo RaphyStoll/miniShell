a887e18eaab7670d2f5ebd487fcd908b613e0e05
RaphyStoll <raphystoll@zenithiac.ch>
2025-03-21 14:13:09 +0100

    add/ fonction utils pour cree ast

--- includes/main/ast.h
+++ includes/main/ast.h
@@ -3,36 +3,31 @@
 # include "colors.h"
 # include "lexing.h"
 
-// enum pour l'ast
-typedef enum e_node_type {
-	AST_COMMAND,  // nœud représentant une commande simple avec ses arguments et redirections
-	AST_PIPE,     // nœud représentant un pipeline
-	AST_LOGICAL,  // nœud pour les opérateurs logiques (&&, ||)
-	AST_GROUP     // nœud pour une expression entre parenthèses
-} t_node_type;
 
 
 //sous structure pour les redirection
 typedef struct s_redirection {
-    t_node_type type;
+    t_type type;
     char *target;                    // Fichier ou délimiteur
     struct s_redirection *next;      // Pour chaîner plusieurs redirections
 } t_redirection;
 
 // structure de base de l'ast
 typedef struct s_node {
-    enum e_node_type type;         // Par exemple : CMD, PIPE, LOGICAL, etc.
+    t_type type;                   // Par exemple : CMD, PIPE, LOGICAL, etc.
     char **args;                   // Tableau d'arguments de la commande
-    struct s_node *child;          // Premier enfant (pour des sous-commandes ou pipelines)
-    struct s_node *brother;        // Frère suivant (pour chaîner des commandes au même niveau)
+    struct s_node *child;          // (Left) Premier enfant (pour des sous-commandes ou pipelines)
+    struct s_node *brother;        // (Right) Frère suivant (pour chaîner des commandes au même niveau)
     t_redirection *redirections;   // Liste des redirections associées à cette commande
 } t_node;
 
-
 //! utils
-t_node *create_node(t_node_type type, char **args);
+t_node *create_node(t_type type, char **args);
 void add_child(t_node *parent, t_node *child);
 void add_brother(t_node *node, t_node *brother);
 void add_redirection(t_node *cmd, t_redirection *redir);
+
+//! debbug
+void print_ast(t_node *node, int indent);
 
 #endif
--- sources/main/parsing/ast/utils.c
+++ sources/main/parsing/ast/utils.c
@@ -1,25 +1,66 @@
 #include "../../../../includes/main/ast.h"
 
 // Crée un nœud de l'AST avec un type donné et des arguments (NULL si non pertinent)
-t_node *create_node(t_node_type type, char **args)
+t_node *create_node(t_type type, char **args)
 {
+	t_node *node;
 
+	node = (t_node *)malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->type = type;
+	node->args = args;
+	node->child = NULL;
+	node->brother = NULL;
+	node->redirections = NULL;
+	return (node);
 }
 
 // Ajoute un enfant à un nœud parent (ex: une commande dans un pipeline)
 void add_child(t_node *parent, t_node *child)
 {
-
+	if (!parent || !child)
+		return ;
+	if (parent->child == NULL)
+		parent->child = child;
+	else
+		add_brother(parent->child, child);
 }
 
 // Ajoute un frère à un nœud (ex: une commande suivante dans un pipeline)
 void add_brother(t_node *node, t_node *brother)
 {
+	t_node *current;
 
+	current = NULL;
+	if (!node || !brother)
+		return ;
+	if (node->brother == NULL)
+		node->brother = brother;
+	else
+	{
+		current = node->brother;
+		while(current->brother != NULL)
+			current = current->brother;
+		current->brother = brother;
+	}
 }
 
 // Ajoute une redirection à un nœud de type commande
 void add_redirection(t_node *cmd, t_redirection *redir)
 {
+	t_redirection *current;
 
+	current = NULL;
+	if (!cmd || !redir)
+		return;
+	if (cmd->redirections == NULL)
+		cmd->redirections = redir;
+	else
+	{
+		current = cmd->redirections;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = redir;
+	}
 }
