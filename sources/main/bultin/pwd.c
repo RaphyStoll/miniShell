c207190af8a175aed3650ec580d9e1b1473f8753
RaphyStoll <raphael.ferreira@ik.me>
2025-04-01 15:23:03 +0200

    add/bult-in pwd

---
+++ sources/main/bultin/pwd.c
@@ -0,0 +1,24 @@
+#include "bultin.h"
+#include "error_code.h"
+
+int builtin_pwd(void)
+{
+	char *cwd;
+
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
+	{
+		perror("pwd");
+		return (GENERIC_ERROR);
+	}
+	printf("%s\n", cwd);
+	free(cwd);
+	return (SUCCESS);
+}
+
+int main ()
+{
+	if (!builtin_pwd())
+		return 1;
+	return 0;
+}
