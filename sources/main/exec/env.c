3478ca3c0cdf4668422b92d99269647db088565a
Charlotte <charlotte.pasquier@bluewin.ch>
2025-04-01 15:49:38 +0200

    add/ expand variables

--- sources/main/exec/env.c
+++ sources/main/exec/env.c
@@ -6,47 +6,30 @@
 /*   By: Charlye <Charlye@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 14:40:45 by Charlye           #+#    #+#             */
-/*   Updated: 2025/04/01 10:20:15 by Charlye          ###   ########.fr       */
+/*   Updated: 2025/04/01 15:49:11 by Charlye          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-void	expand_one_arg(char *arg, t_env *env)
+void	expand_redirections(t_node *node, t_env *env)
+{
+	
+}
+
+char	expand_one_arg(char *arg, t_env *env)
 {
 	char	*result;
 	int		i;
-	
+
 	result = NULL;
 	while (arg[i])
 	{
 		if (arg[i] != '$')
 			result[i] == arg
 	}
-}
 
-/**
- * @brief Expands environment variables in a given node's arguments.
- * 
- * This function iterates through the arguments of a command node and replaces
- * any env variable references (e.g., $VAR) with their corresponding values
- * from the environment list.
- * 
- * @param node The command node containing the arguments to expand.
- * @param env_list environment list used to look up variable values.
- */
-void	expand_variables(t_node *node, t_env *env)
-{
-	char	**args;
-	char	*type;
-	char	*value;
-	int		i;
-
-	i = 0;
-	args = node->args;
-	while (args[i])
-	{
-		if (args[i][0] == '$' && args[i][1])
+			if (args[i][0] == '$' && args[i][1])
 		{
 			type = ft_strdup(&args[i][1]);
 			value = get_env_value(env, type);
@@ -59,4 +42,33 @@ void	expand_variables(t_node *node, t_env *env)
 		}
 		i++;
 	}
-}
+
+/**
+ * @brief Expands variables in command arguments based on quote type.
+ *
+ * Only arguments without quotes or with double quotes will be expanded.
+ * Arguments inside single quotes are left untouched.
+ *
+ * @param node AST command node whose arguments may be expanded.
+ * @param env Environment variable list 
+ */
+void	expand_variables(t_node *node, t_env *env)
+{
+	char	**args;
+	char	*new_arg;
+	int		quote_type;
+	int		i;
+
+	i = 0;
+	args = node->args;
+	while (args[i])
+	{
+		quote_type = node->arg_quotes[i];
+		if (quote_type == QUOTE_NONE || quote_type == QUOTE_DOUBLE)
+		{
+			new_arg = expand_one_arg(args[i], env);
+			free(args[i]);
+			args[i] = new_arg;
+		}
+		i++;
+	}
