196e8be3430485232065ee08e33a043e5974e9c1
RaphyStoll <raphael.ferreira@ik.me>
2025-04-01 19:31:39 +0200

    add/main_Struct t_shell

--- includes/main/minishell.h
+++ includes/main/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Charlye <Charlye@student.42.fr>            +#+  +:+       +#+        */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 15:43:43 by raphalme          #+#    #+#             */
-/*   Updated: 2025/03/31 15:09:41 by Charlye          ###   ########.fr       */
+/*   Updated: 2025/04/01 19:31:16 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,19 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "error_code.h"
+# include "env_struct.h"
+# include "ast_struct.h"
 # include "../../lib/libft/header/libft.h"
 # include "../../lib/libft/header/libft_bonus.h"
 
-//int	main(void);
+typedef struct s_shell
+{
+	t_env		*env;          // Variables d'environnement (liées à export/unset/env)
+	t_node		*ast;          // Arbre de commandes
+	int			last_exit;     // Valeur de $? utilisée partout
+}	t_shell;git 
+
+
+//int	main(int argc, char **argv, char **envp);
 
 #endif
