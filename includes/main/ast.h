0ef1c17de04ed3e05a14a88d26560fd1df78dd7d
RaphyStoll <raphael.ferreira@ik.me>
2025-04-03 16:40:59 +0200

    move/ move des prototypes de free ast dans utils

--- includes/main/ast.h
+++ includes/main/ast.h
@@ -6,7 +6,7 @@
 /*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 19:47:16 by raphaelferr       #+#    #+#             */
-/*   Updated: 2025/03/27 11:37:50 by raphaelferr      ###   ########.fr       */
+/*   Updated: 2025/04/03 10:39:08 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "lexing_struct.h"
 # include <stdlib.h>
 # include "ast_struct.h"
+# include "utils.h"
 
 //! build ast
 t_node	*build_ast(t_token *tokens);
@@ -36,11 +37,4 @@ t_node	*parse_pipe(t_token **tokens);
 //! debbug
 void	print_ast(t_node *node, int indent);
 void	print_ast_debug(t_node *node, int level, const char *label);
-
-//!free
-
-void	free_redirections(t_redirection *redir);
-void	free_args(char **args);
-void	free_all_ast(t_node *ast);
-void	free_node(t_node *node);
 #endif
--- includes/main/utils.h
+++ includes/main/utils.h
@@ -6,7 +6,7 @@
 /*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 09:57:57 by Charlye           #+#    #+#             */
-/*   Updated: 2025/03/25 19:46:09 by raphaelferr      ###   ########.fr       */
+/*   Updated: 2025/04/03 10:38:34 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,21 @@
 # include "lexing_struct.h"
 # include "minishell.h"
 
-// Error handling
+//! Error handling
 void	nbr_error(t_error error_code, char *message);
 t_token	*ft_exit_error(t_token *tokens, t_error code, char *error_token);
 void	print_error_message(t_error code, char *error_token);
 
-//Memory utils
+//! Memory utils
 void	free_all(t_token *token);
 void	free_tokens(t_token *head);
+void free_env(t_env *head);
+
+//! free ast
+
+void	free_redirections(t_redirection *redir);
+void	free_args(char **args);
+void	free_all_ast(t_node *ast);
+void	free_node(t_node *node);
 
 #endif
