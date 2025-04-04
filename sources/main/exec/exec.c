037b9d6e2ebd3d403a99f9262c033d5c79ec0c42
Charlotte <charlotte.pasquier@bluewin.ch>
2025-04-04 18:17:40 +0200

    add/ strucutre fichier dossier exec

--- sources/main/exec/exec.c
+++ sources/main/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: Charlye <Charlye@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 09:20:29 by Charlye           #+#    #+#             */
-/*   Updated: 2025/04/04 17:02:16 by Charlye          ###   ########.fr       */
+/*   Updated: 2025/04/04 18:16:17 by Charlye          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,18 +35,4 @@ int	execute_ast(t_node *ast_node, t_shell *shell)
 		status = 1;
 	shell->last_exit_status = status;
 	return (status);
-}
-
-int	execute_pipe(t_node *pide, t_env *env_list)
-{
-	
-}
-int	execute_logical(t_node *logic, t_env *env_list)
-{
-	
-}
-
-int	execute_subshell(t_node *subshell, t_env *env_list)
-{
-	
 }
