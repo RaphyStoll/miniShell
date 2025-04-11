42b7d67de469b76ab8e936c500666c31c9ebafe2
RaphyStoll <raphael.ferreira@ik.me>
2025-04-11 16:23:07 +0200

    norm / normer les built-in

--- sources/main/bultin/unset.c
+++ sources/main/bultin/unset.c
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/11 16:22:19 by raphaelferr       #+#    #+#             */
+/*   Updated: 2025/04/11 16:22:51 by raphaelferr      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 #include "env_struct.h"
 
-int builtin_unset(t_shell *shell, char **args)
+int	builtin_unset(t_shell *shell, char **args)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(args[i])
+	while (args[i])
 	{
 		unset_env(&(shell->env), args[i]);
 		i++;
