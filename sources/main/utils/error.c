4b9b5969223a619c549bd6a9acdb6ec75ccfd293
RaphyStoll <raphystoll@zenithiac.ch>
2025-04-12 19:03:13 +0000

    fix/ erreur de taille de print

--- sources/main/utils/error.c
+++ sources/main/utils/error.c
@@ -50,24 +50,24 @@ void	print_error_message(t_error_code code, char *error_token)
 	if (code == UNCLOSED_QUOTE)
 	{
 		write(2,
-			"syntax error near unexpected EOF while looking for matching '",
-			61);
+			"syntax error near unexpected EOF while looking for matching\n",
+			60);
 		write(2, error_token, 1);
 		write(2, "'\n", 2);
 	}
 	else if (code == UNCLOSED_PARENTHESIS || code == SYNTAX_ERROR)
 	{
-		write(2, "syntax error near unexpected token '", 30);
+		write(2, "syntax error near unexpected token\n", 35);
 		write(2, error_token, 1);
 		write(2, "'\n", 2);
 	}
 	else if (code == MEMORY_ERROR)
 	{
 		write(2, error_token, ft_strlen(error_token));
-		write(2, " allocation error\n", 18);
+		write(2, " allocation error\n", 17);
 	}
 	else
-		write(2, "unknown error\n", 25);
+		write(2, "unknown error\n", 14);
 }
 
 t_token	*ft_exit_error(t_token *tokens, t_error_code code, char *error_token)
