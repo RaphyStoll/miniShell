0075c7009b181bdf478c674b55d7f6350a33950c
RaphyStoll <raphael.ferreira@ik.me>
2025-04-19 10:07:00 +0200

    fix/changement de la ligique des redirection pour retourner false toujours pour ne pas aller dans l'ast

--- sources/main/parsing/parsing.c
+++ sources/main/parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: raphael <raphael@student.42.fr>            +#+  +:+       +#+        */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 15:32:42 by raphaelferr       #+#    #+#             */
-/*   Updated: 2025/04/12 22:17:41 by raphael          ###   ########.fr       */
+/*   Updated: 2025/04/19 10:06:35 by raphaelferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,16 @@
  * @param tokens Token list to validate.
  * @return true if valid, false otherwise.
  */
+
+ //? handle_redirection_exceptions return false si cela marche
+ //? pour eviter de continuer sur la suite de l'execution
 bool	init_parsing(t_token *tokens)
 {
 	if (!valid_content(tokens))
 		return (false);
 	if ((handle_redirection_exceptions(tokens)))
-		return (true);
+		return (false);
+			return (false);
 	if (!if_valide_token_prev(tokens))
 		return (false);
 	if (!if_valide_token_next(tokens))
