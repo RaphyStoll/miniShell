58f5e0aeb43b9e8d64e29ebb2795039a59c89dbc
RaphyStoll <raphystoll@zenithiac.ch>
2025-05-01 11:22:06 +0200

    fix/leaks exit

--- sources/main/builtin/exit.c
+++ sources/main/builtin/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Charlye <Charlye@student.42.fr>            +#+  +:+       +#+        */
+/*   By: raphalme <raphalme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 16:22:16 by raphaelferr       #+#    #+#             */
-/*   Updated: 2025/04/30 17:23:08 by Charlye          ###   ########.fr       */
+/*   Updated: 2025/05/01 11:21:20 by raphalme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,31 @@ int	builtin_exit(t_shell *shell, char **args)
 	i = 0;
 	ft_putstr_fd("exit\n", 1);
 	if (!args[1])
-		exit(shell->last_exit_status);
+		error_msg(shell, args);
 	if (args[1][0] == '+' || args[1][0] == '-')
 		i++;
 	while (args[1][i])
 	{
 		if (!ft_isdigit(args[1][i]))
 		{
-			ft_putstr_fd("minishell: exit: ", 2);
-			ft_putstr_fd(args[1], 2);
-			ft_putstr_fd(": numeric argument required\n", 2);
+			free_shell(shell);
+			
 			exit(2);
 		}
 		i++;
 	}
 	if (args[2])
-		return (ft_putstr_fd(SHELL_NAME, 2)
+		return (free_shell(shell), ft_putstr_fd(SHELL_NAME, 2)
 			, ft_putstr_fd("exit: too many arguments\n", 2), GENERIC_ERROR);
+	free_shell(shell);
 	exit(ft_atoi(args[1]) % 256);
+}
+
+void error_msg(t_shell *shell, char **args)
+{
+	ft_putstr_fd("minishell: exit: ", 2);
+	ft_putstr_fd(args[1], 2);
+	ft_putstr_fd(": numeric argument required\n", 2);
+	free_shell(shell);
+	exit(shell->last_exit_status);
 }
